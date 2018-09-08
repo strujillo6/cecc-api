c7396a74a89338a802dbbcb2c0267183b8b55c4a
Sergio Trujillo del Valle <trujillo.sergio6@gmail.com>
2018-09-08 13:59:41 -0500

    cambio de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  get 'users/current' => 'user#current'
-  resources :users
+  scope path: 'api/v1' do
+    post 'user_token' => 'user_token#create'
+    get 'users/current' => 'user#current'
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
